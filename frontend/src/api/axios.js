e124d0e366bb762599759c5f19d1803215c73505
Ali Haydoura <alihaydoura3@gmail.com>
2026-01-02 17:37:49 +0200

    Change API base URL to production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ticket-booking-system-api-d9atfsehd8ajddey.germanywestcentral-01.azurewebsites.net/api",
 });
 
 apiClient.interceptors.request.use((config) => {
